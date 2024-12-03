5b1b20485117cbd5815016a08140d67c91664b2e
Chingis Toktamyssov <coktamyssovthingis@gmail.com>
2024-12-02 21:17:08 -0500

    pog

--- user.interface/app.py
+++ user.interface/app.py
@@ -1,57 +1,81 @@
 from flask import Flask, request, jsonify, send_from_directory
-import kociemba
 
 app = Flask(__name__,
             static_url_path='', 
             static_folder='static')
 
+buttonColors = []
+
 @app.route("/")
 def index():
     return send_from_directory("static", 'index.html')
 
+def populateCube(buttonColors):
+    # New list to organize the buttonColors list
+    cube = [[1 for j in range(9)] for i in range(6)]
+    # Hashmap to abbreviate colors into letters, more compact list --> easier to read
+    
+    shortenColor = {'white': 'W', 'blue': 'B', 'red': 'R', 'orange': 'O', 'green': 'G', 'yellow': 'Y'}
+
+    # This brute force is very stupid but easy --> I like :D
+
+    # Populate the white center at index 0
+    for i in range(9):
+        cube[0][i] = shortenColor[buttonColors[i]]
+
+    # Layer 1 Middle Section
+    face = 1
+    counter = 0
+    for i in range(9, 21):
+        cube[face][counter] = shortenColor[buttonColors[i]]
+        counter += 1
+        if counter == 3:
+            counter = 0
+            face += 1
+    
+    # Layer 2 Middle Section
+    face = 1
+    counter = 3
+    for i in range(21, 33):
+        cube[face][counter] = shortenColor[buttonColors[i]]
+        counter += 1
+        if counter == 6:
+            counter = 3
+            face += 1
+
+    # Layer 3 Middle Section
+    face = 1
+    counter = 6
+    for i in range(33, 45):
+        cube[face][counter] = shortenColor[buttonColors[i]]
+        counter += 1
+        if counter == 9:
+            counter = 6
+            face += 1
+
+    # Populate the yellow center at index 5
+    k = 0
+    for i in range(45, 54):
+        cube[5][k] = shortenColor[buttonColors[i]]
+        k += 1
+
+    return cube
+    
+
 # Route to receive the buttonColors list
 @app.route('/submit-colors', methods=['POST'])
 def receive_colors():
+    global buttonColors
+
     data = request.json  # Receive the JSON data sent from JavaScript
-    button_colors = data.get('buttonColors', [])
+    buttonColors = data.get('buttonColors', [])
     
     # Process the button_colors here (save to a file, database, etc.)
-    print("Received buttonColors:", button_colors)
-    
-    # Send a response back to JavaScript
-    return jsonify({"message": "Colors received successfully", "status": "success"})
+    print("Received buttonColors:", buttonColors)
+
+    cube = populateCube(buttonColors)
+    # Print out the new list
+    print("Reorganized buttonColors:", cube)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Map colors from the buttonColors array to the solver format
-color_map = {
-    "white": "U",  # Up face (White)
-    "green": "F",  # Front face (Green)
-    "red": "R",    # Right face (Red)
-    "blue": "B",   # Back face (Blue)
-    "orange": "L", # Left face (Orange)
-    "yellow": "D"  # Down face (Yellow)
-}
-
-# A function that converts the buttonColors list into the format the solver expects
-def convert_to_solver_format(buttonColors):
-    # Mapping of buttonColors (54 items) to the solver's string (54 characters)
-    solver_input = ""
-    
-    for color in buttonColors:
-        solver_input += color_map[color]
-        
-    return solver_input
-
-# Example buttonColors list (you should replace this with the actual list from the layout)
-buttonColors = button_colors
-
-# Convert the buttonColors to the string format expected by the solver
-solver_input = convert_to_solver_format(buttonColors)
-
-# Use the Kociemba solver to get the solution (a sequence of moves)
-solution = kociemba.solve(solver_input)
-
-print("Solution to the Rubik's Cube:")
-print(solution)
