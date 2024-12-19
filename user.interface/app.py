918396456694ede583afcacf496dbf786be7d777
chingistoktamyssov <coktamyssovthingis@gmail.com>
2024-12-19 14:48:36 -0500

    thank fuck it works

--- user.interface/app.py
+++ user.interface/app.py
@@ -8,17 +8,37 @@ app = Flask(__name__,
 # Define a function to map color names to cube notation (U, R, F, D, L, B)
 def map_colors_to_notation(button_colors):
     # This is an example mapping of colors to cube sides
+
     color_map = {
         'white': 'U',
-        'red': 'R',
-        'green': 'F',
+        'red': 'F',
+        'green': 'L',
         'yellow': 'D',
-        'orange': 'L',
-        'blue': 'B'
+        'orange': 'B',
+        'blue': 'R'
     }
-    
-    # Convert the color list to the cube notation (54 characters)
-    cube_state = ''.join([color_map[color] for color in button_colors])
+
+    new_colors = []
+    for i in range(9):
+        new_colors.append(color_map[button_colors[i]])
+    for i in [15, 27, 39]:
+        for j in range(i, i+3):
+            new_colors.append(color_map[button_colors[j]])
+    for i in [12, 24, 36]:
+        for j in range(i, i+3):
+            new_colors.append(color_map[button_colors[j]])
+    for i in range(45, 54):
+        new_colors.append(color_map[button_colors[i]])
+    for i in [9, 21, 33]:
+        for j in range(i, i+3):
+            new_colors.append(color_map[button_colors[j]])
+    for i in [18, 30, 42]:
+        for j in range(i, i+3):
+            new_colors.append(color_map[button_colors[j]])
+
+    cube_state = ''
+    for i in new_colors:
+        cube_state += i
     
     return cube_state
 
@@ -33,14 +53,11 @@ def receive_colors():
     button_colors = data.get('buttonColors', [])
     
     # Process the button_colors here (save to a file, database, etc.)
-    print("Received buttonColors:", button_colors)
-
     cube_state = map_colors_to_notation(button_colors)
-        
+ 
     # Solve the cube using rubik_solver
     solution = sv.solve(cube_state)
     print(solution)
-
 
     return jsonify({'status': 'success', 'message': 'Colors received and processed'})
 
